bf534f48ce17e7ac5378639ce544932b5744adbd
Culver Stone <culver.stone57@gmail.com>
2025-02-02 19:42:27 -0800

    Cont. to start building the message generating tool.

--- generators/adsb_data/app.py
+++ generators/adsb_data/app.py
@@ -1,5 +1,7 @@
-import requests
 import json
+import os
+import requests
+
 
 import numpy as np
 import pandas as pd
@@ -63,16 +65,22 @@ def convert_adsb_json_to_dataframe(aircrafts):
     df = pd.DataFrame(values)
     return df
 
+if os.path.exists('data.json'):
+    ## Get my aircrafts from a file (temporary)
+    aircrafts = open_aircraft_from_file()
+else:
+    ## Pull the aircrafts from adsb and save to file
+    aircrafts = read_adsb_api()
+    save_aircraft_to_file(aircrafts)
 
-## Get my aircrafts from a file (temporary)
-aircrafts = open_aircraft_from_file()
+## Convert the aircrafts into a dataframe
 df = convert_adsb_json_to_dataframe(aircrafts)
 
 ## Get our configuration
 config = get_config()
+
 ## Update our dataframe and include what the current status of our datapoints are
 df["last_update"] = pd.to_datetime('now')
-
 
 ## Find the flights that don't have a flight, we are going to fake a seperate process for them
 na_flight_df = df[df['flight'].isna()]
@@ -80,3 +88,29 @@ na_flight_df = df[df['flight'].isna()]
 ## Find the flights that start with UA, since we are going to use them to pretend we have a process running in the background
 ua_flights_df = df[df['flight'].notna()]
 
+
+## Add a initial status to the dataframe
+ua_flights_df["status"] = config["initial_task"]
+ua_flights_df["system_name"] = config["system_name"]
+ua_flights_df["timestamp"] = pd.to_datetime('now')
+ua_flights_df["initial_time"] = 0
+
+## Build up our log of messages until we are called then purge them
+messages_pending = []
+for index, row in ua_flights_df.iterrows():
+    msg = {}
+    msg["last_update"] = row["last_update"]
+    msg["status"] = row["status"]
+    msg["system_name"] = row["system_name"]
+    msg["timestamp"] = row["timestamp"]
+    msg["flight"] = row["flight"]
+    msg["r_msg"] = row["r"]
+    msg["t_msg"] = row["t"]
+    msg["emergency"] = row["emergency"]
+    msg["category"] = row["category"]
+    msg["lat"] = row["lat"]
+    msg["lon"] = row["lon"]
+    
+    messages_pending.append(msg)
+
+print(messages_pending)
