395805d3a1edf69df32b5e48d9183e71a428f3db
Culver Stone <culver.stone57@gmail.com>
2025-02-15 21:38:29 -0800

    Started putting together a class for parsing the derg files, and trying to make it somewhat generic

--- parser/derg_parser.py
+++ parser/derg_parser.py
@@ -18,11 +18,25 @@ class DergParser:
 
         ## Max number of lines to read (after skipped) into our dataframe
         self.max_lines = None
+        if "max_lines" in kwargs.keys():
+            self.max_lines = kwargs["max_lines"]
+
+        ## So that this doesn't need to get changed when I move this to production, 
+        ##     go ahead and make these parameters
+        self.parts = {
+            "time"   : {"start": 0,  "end": 12, "type": "str"},
+            "meat1"  : {"start": 12, "end": 22, "type": "str"},
+            "jseries": {"start": 22, "end": 31, "type": "jseries"},
+            "meta2"  : {"start": 31, "end": 38, "type": "str"},
+            "meta3"  : {"start": 38, "end": 10, "type": "str"},
+            "msg"    : {"start": 47,},
+        }
 
         ## Allow for input of variable number of arguments and accept them if passed
         # for key, value in kwargs.items():
         #     print(f"Key: {key}, Value: {value}")
         #     self[key] = value
+        ## ============================= End __init__ =============================
 
     def parse(self, file_path):
         """
@@ -36,6 +50,14 @@ class DergParser:
         hour = "00"
         df = {}
 
+        ## Build our dataframe results
+        for key in self.parts.keys():
+            df[key] = []
+        ## Add the two columns for the jseries parts
+        df["jseries_1"] = []
+        df["jseries_2"] = []
+
+        ## Open our file and start looping through the lines
         with open(file_path, 'r') as file:
             line_number = 0
             ## Skip of leading lines
@@ -47,27 +69,49 @@ class DergParser:
                 if line_number > self.first_lines_skip:
                     break
             
-            stop_at = self.max_lines + self.first_lines_skip
+            stop_at = None
+            if self.max_lines is not None:
+                stop_at = self.max_lines + self.first_lines_skip
 
             for line in file:
                 ## TO-DO: comment me out, Print our current line, for trouble shooting
                 print(line)
 
-                ## Check for our first line, since it's not really important
-
                 ## Check if this is our time (hour) line, if so update our hour
-
-                ## Parse the line if it starts with a timestamp 
+                if line.startswith(self.time_line_prefix):
+                    hour = line[3:5]
 
                 ## Parse the lines that start with a blank, thus append it to the previous line
+                if line.startswith(" "):
+                    print("-> Continuation line")
+                ## Parse the line if it starts with a timestamp 
+                else:
+                    print("Message line")
+                    df = self.parseFullLine(line, df)
 
                 ## Increment our line number
                 line_number += 1
 
-                if line_number > stop_at:
+                print(f"hour {hour}")
+
+                if stop_at is not None and line_number > stop_at:
                     break
+
+        print("Print the dataframe results")
+        print(df)
 
         ## Return the dataframe
         return self.dataframe
+        ## =============================== End parse ==============================
+
+    def parseFullLine(self, line, dict):
+        """
+        Parse a full line
+        """
+        for key in self.parts.keys():
+
+
+        return dict
 
 derg_parser = DergParser(max_lines=5)
+derg_parser.parse("fake_derg.derg")
